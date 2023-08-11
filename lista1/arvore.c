7c8ffbec065536d1036b935aab6649486aa7dac4
Nixxye <128294977+Nixxye@users.noreply.github.com>
2023-08-11 18:06:23 -0300

    lista 1 completa

--- lista1/arvore.c
+++ lista1/arvore.c
@@ -27,26 +27,107 @@ void arv_libera (Arvore* a) {
 
 //========= Exercício 2 - pré-ordem ====
 
+void preordem (Arvore *a)
+{
+   if (!verifica_arv_vazia(a))
+   {
+      printf("%c ", a->info);
+      preordem(a->esq);
+      preordem(a->dir);
+   }
+}
 
 //========= Exercício 2 - in-ordem ====
 
+void inordem (Arvore *a)
+{
+   if (!verifica_arv_vazia(a))
+   {
+      preordem(a->esq);
+      printf("%c ", a->info);
+      preordem(a->dir);
+   }
+}
 
 //========= Exercício 2 - pós-ordem ====
 
+void posordem (Arvore *a)
+{
+   if (!verifica_arv_vazia(a))
+   {
+      preordem(a->esq);
+      preordem(a->dir);
+      printf("%c ", a->info);
+   }
+}
 
 //========= Exercício 3 - pertence ====
 
+int pertence_arv (Arvore *a, char c)
+{
+   int r = 0;
+   if (!verifica_arv_vazia(a))
+   {
+      if (a->info == c)
+         return 1;
+      r = pertence_arv(a->esq, c);
+      if (!r)
+         r = pertence_arv(a->dir, c);
+   }
+   return r;
+}
 
 //========= Exercício 4 - conta nós ====
 
+int conta_nos (Arvore* a)
+{
+   int n = 0;
+   if (!verifica_arv_vazia(a))
+   {
+      n++;
+      n += conta_nos(a->dir);
+      n += conta_nos(a->esq);
+   }
+   return n;
+}
 
 //========= Exercício 5 - calcula altura ====
 
-
+int calcula_altura_arvore (Arvore* a)
+{
+   int h = 0;
+   if (!verifica_arv_vazia(a))
+   {
+      if (!verifica_arv_vazia(a->dir) && !verifica_arv_vazia(a->esq))
+      {
+         h = 1;
+         int he = calcula_altura_arvore(a->esq), 
+         hd = calcula_altura_arvore(a->dir);
+         h += hd > he ? hd : he;
+      }
+   }
+   return h;
+}
 
 //========= Exercício 6 - conta folhas ====
 
-
+int conta_nos_folhas(Arvore *a)
+{
+   int f = 0;
+   if (!verifica_arv_vazia(a))
+   {
+      if (verifica_arv_vazia(a->dir) && verifica_arv_vazia(a->esq))
+      {
+         f++;
+      }
+      else
+      {
+         f += conta_nos_folhas(a->dir);
+         f += conta_nos_folhas(a->esq);
+      }
+   }
+   return f;   
+}
 
 int main (int argc, char *argv[]) {
 
@@ -61,6 +142,16 @@ int main (int argc, char *argv[]) {
      )
    );	
 
+   // preordem(a);
+   // printf("\n");
+   // inordem(a);
+   // printf("\n");
+   // posordem(a);
+
+   // printf("%d\n", pertence_arv(a, 'l'));
+   // printf("%d\n", conta_nos(a));
+   // printf("%d\n", calcula_altura_arvore(a));
+   printf("%d\n", conta_nos_folhas(a));
    arv_libera (a);
 
    return 0;
